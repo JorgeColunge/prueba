f7f4ec3cb16cc8d55c72386ebf56002557c0433c
Jorge <cto.axiomarobotics@gmail.com>
2024-05-17 09:01:40 -0500

    Agregue aplicacion perfil, ruta y view

--- server/urls.py
+++ server/urls.py
@@ -19,6 +19,7 @@ from django.conf.urls import url,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r'^$', include('home.urls'))
+    url(r'^$', include('home.urls')),
+    path('perfil/', include('perfil.urls')),
     
 ]
